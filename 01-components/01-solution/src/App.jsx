474ca02a0e8d7e8baf8bee9348cc52775ad52b47
Amund Tenstad <github@amund.io>
2020-10-13 17:42:17 +0200

    Create solution for 01-components

--- 01-components/01-solution/src/App.jsx
+++ 01-components/01-solution/src/App.jsx
@@ -1,3 +1,10 @@
-const App = () => null
+import React from 'react'
+import Stars from './Stars'
+
+const App = () => <>
+    <Stars /><Stars />
+    <h1><Stars /></h1>
+    <Stars /><Stars />
+</>
 
 export default App
