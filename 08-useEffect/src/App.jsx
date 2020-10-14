d61b65c6a654c1ea874e9fcd162fc89cfb94d606
EIVINDSEN Joachim <joachim.eivindsen@soprasteria.com>
2020-10-14 15:50:38 +0200

    Remove code

--- 08-useEffect/src/App.jsx
+++ 08-useEffect/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState} from 'react';
 
 const App = () => {
-    const [toggle, setToggle] = useState(true);
     const [random, setRandom] = useState(0);
 
     //This function updates the randomm value every 3 seconds, causing the page to re-render
@@ -10,16 +9,10 @@ const App = () => {
             setRandom(Math.random())
         }, 3000);
       });
-      
-
-     useEffect(() => {
-        console.log(toggle)
-    },[toggle])
-
 
     return (
         <div>
-            <button onClick={() => setToggle(toggle => !toggle)}>Toggle</button>
+            <p>{random}</p>
         </div>
     )
 }
